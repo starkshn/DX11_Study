1ac34e631696ed2bb23c4b03c1901850639ba9fa
DESKTOP-6QQQTSE\김남영 <starkshn97@gmail.com>
2023-08-22 18:00:03 +0900

    [장치 초기화] 1. device, deviceContext, swapChain 장치 초기화

--- GameCoding/Game.cpp
+++ GameCoding/Game.cpp
@@ -13,11 +13,13 @@ Game::~Game()
 
 void Game::Init(HWND hWnd)
 {
-	_hwnd = hWnd;
+	_hWnd = hWnd;
 	_width = GWinSizeX;
 	_height = GWinSizeY;
 
 	// TODO
+	CreateDeviceAndSwapChain();
+	
 }
 
 void Game::Update()
@@ -28,4 +30,50 @@ void Game::Update()
 void Game::Render()
 {
 
+}
+
+// 800 x 600
+void Game::CreateDeviceAndSwapChain()
+{
+	DXGI_SWAP_CHAIN_DESC desc;
+	ZeroMemory(&desc, sizeof(desc));
+	{
+		desc.BufferDesc.Width = _width; // 800
+		desc.BufferDesc.Height = _height; // 600
+		desc.BufferDesc.RefreshRate.Numerator = 60; // 화면 주사율
+		desc.BufferDesc.RefreshRate.Denominator = 1; // 분모
+		desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; // 각각 8비트
+		desc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+		desc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+
+		// 아래 두부분은 조금 중요하다.
+		// Count : 멀티 셈플링과 관련된 문제이다.
+		desc.SampleDesc.Count = 1;
+		desc.SampleDesc.Quality = 0;
+
+		// 아래 3개 중요하다. 
+		// 버퍼 어떻게 사용할 것인가?
+		// 아래의 매크로는 최종 결과물을 그리는데 사용하겠다.
+		desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		desc.BufferCount = 1; // 후면 버퍼 개수
+		desc.OutputWindow = _hWnd; // 어떤 윈도우 핸들 설정?
+		desc.Windowed = true;
+		desc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	}
+
+	HRESULT hr = ::D3D11CreateDeviceAndSwapChain
+	(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		nullptr, 0,
+		nullptr, 0,
+		D3D11_SDK_VERSION,
+		&desc,
+		_swapChain.GetAddressOf(),
+		_device.GetAddressOf(),
+		nullptr, 
+		_deviceContext.GetAddressOf()
+	);
+
+	CHECK(hr);
 }
--- GameCoding/Game.h
+++ GameCoding/Game.h
@@ -11,11 +11,18 @@ public:
 	void Render();
 
 private:
-	HWND	_hwnd;
+	void CreateDeviceAndSwapChain();
+
+private:
+	HWND	_hWnd;
 	uint32	_width = 0;
 	uint32	_height = 0;
 
 	// DX
 private:
-	
+	// ID3D 는 COM 객체이다. (쌩포인터 X) 
+	// wrl.h 에 정의(선언)
+	ComPtr<ID3D11Device> _device = nullptr;
+	ComPtr<ID3D11DeviceContext> _deviceContext = nullptr;
+	ComPtr<IDXGISwapChain> _swapChain = nullptr;
 };
