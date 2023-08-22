033b136e2da7978cd8d2079c02a3552926a32673
DESKTOP-6QQQTSE\김남영 <starkshn97@gmail.com>
2023-08-22 16:55:18 +0900

    [프레임 워크 구현] 1. header, libraries path 설정

---
+++ GameCoding/Game.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "Game.h"
+
+Game::Game()
+{
+
+}
+
+Game::~Game()
+{
+
+}
+
+void Game::Init(HWND hWnd)
+{
+	_hwnd = hWnd;
+	_width = GWinSizeX;
+	_height = GWinSizeY;
+
+	// TODO
+}
+
+void Game::Update()
+{
+
+}
+
+void Game::Render()
+{
+
+}
